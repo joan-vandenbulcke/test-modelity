66aff7fded26ab44a95986ce7c50f9c92d0cbe78
joan-vandenbulcke <vandenbulckejoan@gmail.com>
2024-09-30 15:49:19 +0200

    feat: add msw and browser config

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,16 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./assets/index.css";
 
-createApp(App).mount("#app");
+async function enableMocking() {
+  if (process.env.NODE_ENV !== "development") {
+    return;
+  }
+
+  const { worker } = await import("./mocks/browser");
+
+  return worker.start();
+}
+
+enableMocking().then(() => {
+  createApp(App).mount("#app");
+});
