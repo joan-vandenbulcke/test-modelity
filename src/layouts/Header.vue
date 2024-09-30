c95ad554cb319584fe1df0cd94ca12ebb79061e3
joan-vandenbulcke <vandenbulckejoan@gmail.com>
2024-09-30 18:29:18 +0200

    style: update icon

--- src/layouts/Header.vue
+++ src/layouts/Header.vue
@@ -5,7 +5,7 @@
     <div
       class="flex flex-col items-center px-2 md:px-10 2xl:px-0 lg:container lg:m-auto pt-10"
     >
-      <h1 class="text-2xl mb-6">Calculez vos itinéraires avec</h1>
+      <h1 class="2xl:text-2xl text-xl mb-6">Calculez vos itinéraires avec</h1>
       <div ref="logo" class="logo-container">
         <img class="w-60" src="./../assets/images/logo.png" alt="image" />
       </div>
