808d2df1930abc7d8ffafff5f515929df147bd2d
joan-vandenbulcke <vandenbulckejoan@gmail.com>
2024-09-30 17:21:42 +0200

    test: add e2e test on itineraries search

---
+++ tests/App.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from "@playwright/test";
+
+test("test", async ({ page }) => {
+  await page.goto("http://localhost:5173/");
+  await page.getByPlaceholder("Point de départ").click();
+  await page.getByPlaceholder("Point de départ").fill("Anthon");
+  await page.getByPlaceholder("Point d'arrivée").click();
+  await page.getByPlaceholder("Point d'arrivée").fill("Lyon");
+  await page
+    .getByRole("button", { name: "Rechercher les itinéraires" })
+    .click();
+  await expect(page.getByText('{ "1": { "color": "#5394cf')).toBeVisible();
+});
